136a5a9a0bee8ae3d4727cf4f810d1d7de4821af
Infinitemarcus <marcus.natrielli@outlook.com>
2024-01-18 11:52:06 -0300

    another another test

--- tests/utils/commands.test.ts
+++ tests/utils/commands.test.ts
@@ -1,5 +1,9 @@
 describe('messageStartsWithPrefix tests', () => {
-  it('should', () => {
+  it('should return success', () => {
     expect(1).toBe(1);
+  });
+
+  it('should return failed', () => {
+    expect(2).toBe(1);
   });
 });
