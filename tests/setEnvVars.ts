372787c89e676effc381d43e3de3b9885f6be5fb
Infinitemarcus <marcus.natrielli@outlook.com>
2024-01-18 12:06:24 -0300

    Final test with real jest tests

---
+++ tests/setEnvVars.ts
@@ -0,0 +1,4 @@
+import { EnvNames } from '../src/constants';
+
+process.env.ENV_NAME = EnvNames.Test;
+process.env.COMMAND_PREFIX = '!';
--- tests/utils/commands.test.ts
+++ tests/utils/commands.test.ts
@@ -1,9 +1,21 @@
+import { messageStartsWithPrefix } from '../../src/utils';
+
 describe('messageStartsWithPrefix tests', () => {
-  it('should return success', () => {
-    expect(1).toBe(1);
+  it('should return success by prefix found in message begin', () => {
+    const message = '!discord';
+    const hasPrefix = messageStartsWithPrefix(message);
+    expect(hasPrefix).toBeTruthy();
   });
 
-  it('should return failed', () => {
-    expect(2).toBe(1);
+  it('should return fail by no prefix found in message', () => {
+    const message = 'discord';
+    const hasPrefix = messageStartsWithPrefix(message);
+    expect(hasPrefix).toBeFalsy();
+  });
+
+  it('should return fail by prefix not found in message begin', () => {
+    const message = 'dis!cord !hello';
+    const hasPrefix = messageStartsWithPrefix(message);
+    expect(hasPrefix).toBeFalsy();
   });
 });
